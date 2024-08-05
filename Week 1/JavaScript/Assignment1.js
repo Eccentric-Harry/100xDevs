ae2a00eee7b83009dfec01bd89253db822e75454
Eccentric Harry <harinathrao13@gmail.com>
2024-08-05 23:21:19 +0530

    Assignment 1

---
+++ Week 1/JavaScript/Assignment1.js
@@ -0,0 +1,32 @@
+//Create a function that takes an array of objects as input and returns the users whose age is more than 18.
+
+const users = [
+    {
+        name: "Hari",
+        age: 20
+    },
+    {
+        name: "John",
+        age: 17
+    },
+    {
+        name: "Mike",
+        age: 25
+    },  
+    {
+        name: "Smith",
+        age: 15
+    }
+]
+
+function canVote(users){
+    let result = [];
+    for(let i = 0; i < users.length; i++){
+        if(users[i].age > 18){
+            result.push(users[i].name);
+        }
+    }
+    return result;
+}
+
+console.log(canVote(users));
